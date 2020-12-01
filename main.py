73b59e3b9529044962935e0f7c9bf71f965ed849
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2020-12-01 22:23:43 +0200

    Impemented adding password feature + aes cfb password encryption

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from Crypto.Cipher import AES
 import sqlite3
 
+
 class PasswordManagerUtil:
     def __init__(self, password_manager):
         self.password_manager = password_manager
-        self.connection = sqlite3.connect('pwmanager.db')
+        self.connection = sqlite3.connect('password_manager.db')
         self.cursor = self.connection.cursor()
+        self.__iv = b'1122334455667788'
+
+    @staticmethod
+    def __apply_padding(string, value):
+        while len(string) % 16 != 0:
+            string += value
+        return string
 
     def add_password(self, password_manager, website, username, password):
-        pass
+        if self.password_manager != password_manager:
+            print('Wrong password! Try again!')
+            return
+
+        key = PasswordManagerUtil.__apply_padding(password_manager, '\n').encode('utf8')
+        cipher = AES.new(key, mode=AES.MODE_CFB, iv=self.__iv)
+        encrypted_password = cipher.encrypt(password.encode('utf8'))
+
+        self.cursor.execute('INSERT INTO password_manager (website, username, password) VALUES (?, ?, ?)', (website, username, encrypted_password))
+        self.connection.commit()
 
     def get_password(self, password_manager, website):
         pass
@@ -27,3 +44,4 @@ class PasswordManagerUtil:
 
     def print_help(self):
         pass
+
