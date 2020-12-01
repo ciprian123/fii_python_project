28b6d3a02826143ee2f9b6fac3d1689735777d7d
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2020-12-02 01:18:33 +0200

    Implemented reset master password functionality + added master password param + some changes in code

--- main.py
+++ main.py
@@ -66,7 +66,6 @@ class PasswordManagerUtil:
             return
         print('LISTING PASSWORDS... ')
         rows = self.cursor.execute('SELECT website, username, password FROM password_manager').fetchall()
-        counter = 0
         for row in list(rows):
             cipher = AES.new(self.key, mode=AES.MODE_CFB, iv=self.__iv)
             decrypted_password = cipher.decrypt(row[2]).decode('utf8')
@@ -87,12 +86,39 @@ class PasswordManagerUtil:
         self.connection.commit()
         print('DATA DELETED SUCCESSFULLY!')
 
+    def update_master_password(self, master_password, new_master_password):
+        if self.master_password != master_password:
+            print('Wrong password! Try again!')
+            return
+        # when we change the master password we must re encrypt all data with the new password
+        new_key = PasswordManagerUtil.__apply_padding(new_master_password, '\n').encode('utf8')
+        decrypted_list = []
+        rows = self.cursor.execute('SELECT website, username, password FROM password_manager').fetchall()
+        if len(rows) > 0:
+            for row in rows:
+                cipher_dec = AES.new(self.key, mode=AES.MODE_CFB, iv=self.__iv)
+                decrypted_password = cipher_dec.decrypt(row[2]).decode('utf8')
+                decrypted_list.append([row[0], row[1], decrypted_password])
+        self.key = new_key
+        self.master_password = new_master_password
+        self.cursor.execute('UPDATE manager SET master_password = ?', (new_master_password, ))
+        self.connection.commit()
+
+        self.cursor.execute('DELETE FROM password_manager')
+        self.connection.commit()
+
+        for decrypted_row in decrypted_list:
+            print(decrypted_row)
+            self.add_password(self.master_password, decrypted_row[0], decrypted_row[1], decrypted_row[2])
+
     def print_help(self):
         print('`-add` command has the following parameters: <website> <username> <password>')
         print('`-get` command has the following parameters: <website>')
         print('`-update` command has the following parameters: <website> <username> <new_password>')
         print('`-remove` command has the following parameters: <website>')
         print('`-list` command has no parameters')
+        print('`-reset` command has no parameters')
+        print('`-change_master_password` command has no parameters')
         print('`-help` command has no parameters')
 
 
@@ -115,21 +141,34 @@ if __name__ == '__main__':
     _master_password = bind_master_password()
     pw_manager = PasswordManagerUtil(_master_password)
 
-    if len(sys.argv) == 2:
-        if sys.argv[1] == '-list':
-            pw_manager.list_passwords(_master_password)
-        elif sys.argv[1] == '-help':
+    if len(sys.argv) == 3:
+        if sys.argv[2] == '-list':
+            pw_manager.list_passwords(sys.argv[1])
+        elif sys.argv[2] == '-help':
             pw_manager.print_help()
-    elif len(sys.argv) == 3:
-        if sys.argv[1] == '-get':
-            print(sys.argv[2])
-            pw_manager.get_password(_master_password, sys.argv[2])
-        elif sys.argv[1] == '-remove':
-            pw_manager.remove_password(_master_password, sys.argv[2])
-    elif len(sys.argv) == 5:
-        if sys.argv[1] == '-add':
-            pw_manager.add_password(_master_password, sys.argv[2], sys.argv[3], sys.argv[4])
-        elif sys.argv[1] == '-update':
-            pw_manager.update_password(_master_password, sys.argv[2], sys.argv[3], sys.argv[4])
+        elif sys.argv[2] == '-reset':
+            pw_manager.delete_all_passwords(sys.argv[1])
+        elif sys.argv[2] == '-change_master_password':
+            if _master_password == sys.argv[1]:
+                new_master_password = input('Enter the new master password: ')
+                pw_manager.update_master_password(sys.argv[1], new_master_password)
+            else:
+                print('Wrong password! Try again!')
+        else:
+            pw_manager.print_help()
+    elif len(sys.argv) == 4:
+        if sys.argv[2] == '-get':
+            pw_manager.get_password(sys.argv[1], sys.argv[3])
+        elif sys.argv[2] == '-remove':
+            pw_manager.remove_password(sys.argv[1], sys.argv[3])
+        else:
+            pw_manager.print_help()
+    elif len(sys.argv) == 6:
+        if sys.argv[2] == '-add':
+            pw_manager.add_password(sys.argv[1], sys.argv[3], sys.argv[4], sys.argv[5])
+        elif sys.argv[2] == '-update':
+            pw_manager.update_password(sys.argv[1], sys.argv[3], sys.argv[4], sys.argv[5])
+        else:
+            pw_manager.print_help()
     else:
         pw_manager.print_help()
