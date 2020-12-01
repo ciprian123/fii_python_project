46fd1df8534323a30646ce9b67464e61cb110ed5
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2020-12-01 22:32:02 +0200

    Implemented get password feature + small code improvements

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class PasswordManagerUtil:
         self.password_manager = password_manager
         self.connection = sqlite3.connect('password_manager.db')
         self.cursor = self.connection.cursor()
+        self.key = PasswordManagerUtil.__apply_padding(password_manager, '\n').encode('utf8')
         self.__iv = b'1122334455667788'
 
     @staticmethod
@@ -20,15 +21,22 @@ class PasswordManagerUtil:
             print('Wrong password! Try again!')
             return
 
-        key = PasswordManagerUtil.__apply_padding(password_manager, '\n').encode('utf8')
-        cipher = AES.new(key, mode=AES.MODE_CFB, iv=self.__iv)
+        cipher = AES.new(self.key, mode=AES.MODE_CFB, iv=self.__iv)
         encrypted_password = cipher.encrypt(password.encode('utf8'))
 
         self.cursor.execute('INSERT INTO password_manager (website, username, password) VALUES (?, ?, ?)', (website, username, encrypted_password))
         self.connection.commit()
 
     def get_password(self, password_manager, website):
-        pass
+        if self.password_manager != password_manager:
+            print('Wrong password! Try again!')
+            return
+        print('USERNAME AND PASSWORDS FOR WEBSITE:', website)
+        cipher = AES.new(self.key, mode=AES.MODE_CFB, iv=self.__iv)
+        rows = self.cursor.execute('SELECT username, password FROM password_manager WHERE website = ?', (website, ))
+        for row in rows:
+            decrypted_password = cipher.decrypt(row[1]).decode('utf8')
+            print(f'USERNAME: {row[0]}  :  PASSWORD: {decrypted_password}')
 
     def update_password(self, password_manager, website, username, password):
         pass
@@ -45,3 +53,9 @@ class PasswordManagerUtil:
     def print_help(self):
         pass
 
+
+if __name__ == '__main__':
+    pw_manager = PasswordManagerUtil('112233')
+    # pw_manager.add_password('112233', 'gmail.com', 'ciprian.ursulean5@gmail.com', '12311223344556677')
+    # pw_manager.add_password('112233', 'github.com', 'ciprian.ursulean5@gmail.com', 'githubhehehe')
+    pw_manager.get_password('112233', 'github.com')
